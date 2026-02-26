b1aa48921720edc2fae0239c39c7dd724c1686f8
ChandipriyaNimmala <nc2305@srmist.edu.in>
2026-02-26 15:43:57 +0530

    UC2: Print a Hardcoded Palindrome Result Goal: Display whether a hardcoded string is a palindrome. Actor: User Flow: Program starts Hardcoded string is checked Result is printed Program exits
    Key Concepts used in UC2:
    Class – In Java, even the simplest program must be written inside a class, as a class acts as a container to hold program logic.
    Main Method – Entry point of the Java application with the signature
     public static void main(String[] args).
    Static Keyword – Allows the JVM to invoke the main() method without creating an object of the class.
    String – A built-in Java class used to store and manipulate textual data. Here, it stores the word to be checked for a palindrome.
    String Literal – Text enclosed within double quotes ('madam'), stored in the String constant pool.
    Conditional Statement (if-else) – Used to evaluate whether the given string satisfies the palindrome condition.

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,20 @@
 public class PalindromeCheckerApp {
-    public static void main (){
-        System.out.println(" Hello!! ");
-        System.out.println(" Palindrome Checker App");
-        System.out.println(" Welcome ");
+    public static void main () {
+        String input= "madam";
+        boolean isPalindrome = true;
+        int n = input.length() - 1;
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome)
+        {
+            System.out.println(input + " Is a Palindrome. ");
+        }
+        else {
+            System.out.println(input + " Is not a palindrome ");
+        }
     }
 }
-git
