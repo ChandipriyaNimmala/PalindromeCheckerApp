abb4fbf3e9c519b38c0d0bb780a8b03a23a56735
ChandipriyaNimmala <nc2305@srmist.edu.in>
2026-02-26 16:09:41 +0530

    UC3: Palindrome Check Using String Reverse Goal: Check whether a string is a palindrome by reversing it. Flow: Reverse string using loop Compare original and reversed Display result Key Concepts used in UC3: Loop (for loop) – Used to iterate through the characters of the string in reverse order. String Immutability – In Java, String objects are immutable; every modification creates a new String object. String Concatenation (+) – Used to build the reversed string character by character (introduced here to later discuss drawbacks). equals() Method – Used to compare the actual content of two String objects instead of memory references. Data Structure: String

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,18 @@
 public class PalindromeCheckerApp {
     public static void main (){
-        System.out.println(" Hello!! ");
-        System.out.println(" Palindrome Checker App");
-        System.out.println(" Welcome ");
+        String original = "madam";
+        String reversed = "";
+
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
     }
 }
-git
