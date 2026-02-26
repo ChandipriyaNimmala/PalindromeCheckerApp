9a5a152c761652c750340b3916ff9f7ab4f7153e
ChandipriyaNimmala <nc2305@srmist.edu.in>
2026-02-26 14:37:33 +0530

    UC1: Application Entry & Welcome Message Goal: Display a welcome message and app details at startup. Actor: User Flow: Program starts. JVM invokes the main() method. Application name is displayed. Application version is displayed. Program continues to next use case or exits. Key Concepts used in UC1: Class – Acts as a container for the Palindrome Checker application logic. Main Method – Entry point of the Java application with the signature  public static void main(String[] args). Static Keyword – Allows the JVM to invoke the main() method without creating an object. Console Output – System.out.println() is used to display messages on the console. Application Flow Control – Defines the startup behavior before palindrome processing begins.

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main (){
+        System.out.println(" Hello!! ");
+        System.out.println(" Palindrome Checker App");
+        System.out.println(" Welcome ");
+    }
+}
+git
