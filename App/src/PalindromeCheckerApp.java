022b005c80b16fa7780fdfa11fd677a1c0e3445b
ChandipriyaNimmala <nc2305@srmist.edu.in>
2026-02-26 16:26:12 +0530

    UC4: Character Array Based Palindrome Check Goal: Convert string to character array and compare characters. Flow: Convert string to char[] Use two-pointer approach Compare start & end characters Key Concepts used in UC4: Character Array (char[]) – A primitive array used to store individual characters of a string for index-based access. Array Indexing – Accessing elements using index positions starting from 0. Two-Pointer Technique – One pointer starts from the beginning and the other from the end, reducing unnecessary comparisons. Time Complexity Awareness – Demonstrates efficient comparison without creating extra objects. Data Structure: char[]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,29 @@
 public class PalindromeCheckerApp {
     public static void main (){
-        System.out.println(" Hello!! ");
-        System.out.println(" Palindrome Checker App");
-        System.out.println(" Welcome ");
+        String input = "madam";
+
+        // Convert string to character array
+        char[] arr = input.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        // Result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
     }
 }
-git
